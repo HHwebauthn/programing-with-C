23fb65a73d119a95b6b88e6816bb4ad7cd198e49
Hunter Hayward <Hunter@webauthn.works>
2019-10-17 11:47:23 -0600

    first program

---
+++ applications/small/small.cpp
@@ -0,0 +1,11 @@
+
+#include <iostream> //library stream I/O. this is needed to gain access to library
+
+int main()
+{
+    std::cout << "Hello World!\n";
+	std::cout << "My name is Hunter and this is my first application\n";
+	std::cout << 2 + 2;
+
+}
+
